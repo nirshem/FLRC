392904c7657460fb4848743dd2f58883d4866cda
Nir Shemesh <nirshem64@gmail.com>
2025-11-25 17:51:12 +0200

    Update FLRC transmission settings in Settings.h

--- FLRC/FLRC_TX/52_FLRC_Transmitter/Settings.h
+++ FLRC/FLRC_TX/52_FLRC_Transmitter/Settings.h
@@ -19,13 +19,13 @@
 #define LORA_DEVICE DEVICE_SX1280                         //we need to define the device we are using  
 
 //FLRC Modem Parameters
-const uint32_t Frequency = 2445000000;                    //frequency of transmissions
+const uint32_t Frequency = 2440000000;                    //frequency of transmissions
 const int32_t Offset = 0;                                 //offset frequency for calibration purposes
 
 const uint8_t BandwidthBitRate = FLRC_BR_1_300_BW_1_2;    //FLRC bandwidth and bit rate, 1.3Mbs
 const uint8_t CodingRate = FLRC_CR_1_0;                   //FLRC coding rate
 const uint8_t BT = RADIO_MOD_SHAPING_BT_1_0;              //FLRC BT
-const uint32_t Syncword = 0x01234567;                     //FLRC uses syncword
+const uint32_t Syncword = 0x1;                     //FLRC uses syncword
 
-const int8_t TXpower  = 0;                                //power for transmissions in dBm
+const int8_t TXpower  = 20;                                //power for transmissions in dBm
 const uint16_t packet_delay = 1000;                       //mS delay between packets
